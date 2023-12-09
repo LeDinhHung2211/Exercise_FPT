cdd98c85c98ba3cf5b7fa641f16603c9281a72f1
HunHun <hungle22112001@gmail,com>
2023-12-09 14:12:35 +0700

    Hung_edited

---
+++ Exercise_1/Debug_RAM/src/main.d
@@ -0,0 +1,19 @@
+src/main.o: ../src/main.c \
+ C:/Users/hungl/OneDrive/Desktop/Exercise_FPT/Exercise_1/include/device_registers.h \
+ C:/Users/hungl/OneDrive/Desktop/Exercise_FPT/Exercise_1/include/s32_core_cm4.h \
+ C:/Users/hungl/OneDrive/Desktop/Exercise_FPT/Exercise_1/include/S32K144.h \
+ C:/Users/hungl/OneDrive/Desktop/Exercise_FPT/Exercise_1/include/S32K144_features.h \
+ C:/Users/hungl/OneDrive/Desktop/Exercise_FPT/Exercise_1/include/devassert.h \
+ C:/Users/hungl/OneDrive/Desktop/Exercise_FPT/Exercise_1/include/clocks_and_modes.h
+
+C:/Users/hungl/OneDrive/Desktop/Exercise_FPT/Exercise_1/include/device_registers.h:
+
+C:/Users/hungl/OneDrive/Desktop/Exercise_FPT/Exercise_1/include/s32_core_cm4.h:
+
+C:/Users/hungl/OneDrive/Desktop/Exercise_FPT/Exercise_1/include/S32K144.h:
+
+C:/Users/hungl/OneDrive/Desktop/Exercise_FPT/Exercise_1/include/S32K144_features.h:
+
+C:/Users/hungl/OneDrive/Desktop/Exercise_FPT/Exercise_1/include/devassert.h:
+
+C:/Users/hungl/OneDrive/Desktop/Exercise_FPT/Exercise_1/include/clocks_and_modes.h:
